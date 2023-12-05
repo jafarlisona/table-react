252860280d11a9e00d5605f8bab43616e9425e66
jafarlisona <147070818+jafarlisona@users.noreply.github.com>
2023-12-05 20:03:21 +0400

    Add files via upload

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -4,12 +4,20 @@ import "./index.css";
 function Search() {
   const [inputValue, setInputValue] = useState("");
   const [seacrh, setSeacrh] = useState([]);
+  const [categoryApi, setCategoryApi] = useState([]);
+  const [category, setCategory] = useState("");
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    // fetch("https://jsonplaceholder.typicode.com/users")
+    fetch("https://northwind.vercel.app/api/products")
       .then((res) => res.json())
       .then((data) => setSeacrh(data));
+    fetch("https://northwind.vercel.app/api/categories")
+      .then((res) => res.json())
+      .then((data) => setCategoryApi(data));
   }, []);
-
+  function handleCategory(id) {
+    setCategory(id);
+  }
   return (
     <>
       <input
@@ -17,31 +25,38 @@ function Search() {
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
       />
+      {categoryApi.map((item) => (
+        <button key={item.id} onClick={()=>handleCategory(item.id)}>{item.name}</button>
+      ))}
+      <button onClick={() => handleCategory("")}>All</button>
       <table>
         <thead>
           <tr>
-            <th>Id</th>
+            <th>Category</th>
             <th>Name</th>
-            <th>Username</th>
+            {/* <th>Username</th>
             <th>Email</th>
-            <th>Phone</th>
+            <th>Phone</th> */}
           </tr>
         </thead>
         <tbody>
-            {/* filterle yazmaq lazimdi ,cunki render coxlu gedir.includes filterin icinde olmalidi */}
+          {/* filterle yazmaq lazimdi ,cunki render coxlu gedir.includes filterin icinde olmalidi */}
           {seacrh.map((x) => {
             if (
-              x.name.toLowerCase().includes(inputValue.toLowerCase()) ||
-              x.username.toLowerCase().includes(inputValue.toLowerCase()) ||
-              x.email.toLowerCase().includes(inputValue.toLowerCase()) ||
-              x.phone.toLowerCase().includes(inputValue.toLowerCase())) {
+              x.name.toLowerCase().includes(inputValue.toLowerCase()) &&
+              x.categoryId.toString().includes(category)
+              // x.name.toLowerCase().includes(inputValue.toLowerCase()) ||
+              // x.username.toLowerCase().includes(inputValue.toLowerCase()) ||
+              // x.email.toLowerCase().includes(inputValue.toLowerCase()) ||
+              // x.phone.toLowerCase().includes(inputValue.toLowerCase())) {
+            ) {
               return (
                 <tr key={x.id}>
-                  <td>{x.id}</td>
+                  <td>{x.categoryId}</td>
                   <td>{x.name}</td>
-                  <td>{x.username}</td>
+                  {/* <td>{x.username}</td>
                   <td>{x.email}</td>
-                  <td>{x.phone}</td>
+                  <td>{x.phone}</td> */}
                 </tr>
               );
             }
